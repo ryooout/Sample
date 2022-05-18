55f4b396e559e463bf1bcc81b59fb495b3d3d1ff
ryooout <ryota01010104@gmail.com>
2022-05-18 19:20:41 +0900

    firstcommit

---
+++ Assets/CarController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+    Vector2 startPos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            //マウスをクリックした座標
+            this.startPos = Input.mousePosition;
+        }
+        //クリックを辞めたときの処理
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            //2点間の距離
+            float swipeLength = endPos.x - startPos.x;
+            this.speed = swipeLength / 500f;
+            GetComponent<AudioSource>().Play();
+        }
+        transform.Translate(this.speed, 0, 0);//移動
+        this.speed *= 0.98f;
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{ 
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+    void Start()
+    {
+        //オブジェクトの情報を獲得
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    /// <summary>
+    /// 2点間の距離のプログラム
+    /// </summary>
+    void Update()
+    {
+        
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+          //2点間の距離が0より大きければ
+        if (length >= 0)
+        {
+            this.distance.GetComponent<Text>().text = "ゴールまであと" + length.ToString("F2") + "m";
+        }
+        //0より小さければ
+        else
+        {
+            this.distance.GetComponent<Text>().text = "ゲームオーバー";
+        }
+    }
+}
